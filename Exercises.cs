7c9fd2661a05b878b460cc6bf788015b5efad560
Ryan Cornett <ryandcornett@gmail.com>
2023-08-03 16:05:21 -0400

    Initial commit

---
+++ Exercises.cs
@@ -0,0 +1,62 @@
+﻿namespace Mosh.ControlFlow
+{
+    public class Exercises
+    {
+        public void Exercise1()
+        {
+            int DivisibleByThree()
+            {
+                int count = 0;
+                for (int i = 0; i < 101; i++)
+                {
+                    if (i % 3 == 0)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+            Console.WriteLine("How many numbers between 1 and 100 are divisible by 3 with no remainder?");
+            Console.WriteLine($"Answer: {DivisibleByThree()}");
+        }
+
+        public void Exercise2()
+        {
+            int sum = 0;
+            Console.Write("Enter a number or 'ok' to exit: ");
+            string userInput = Console.ReadLine();
+            while (userInput != "ok") 
+            {
+                int number = Int32.Parse(userInput);
+                sum += number;
+                Console.WriteLine($"The sum of all numbers you have entered is {sum}");
+                Console.Write("Enter a number or 'ok' to exit: ");
+                userInput = Console.ReadLine();
+            }
+        }
+
+        public void Exercise3()
+        {
+            Console.Write("Enter an integer to compute its factorial: ");
+            int userInput = Int32.Parse(Console.ReadLine());
+            int placeholder = userInput;
+            int factorial = 1;
+            while (userInput != 1) 
+            {
+                factorial = factorial * userInput;
+                userInput--;
+            }
+            Console.WriteLine($"{placeholder}! = {factorial}.");
+        }
+
+        public void Exercise4()
+        {
+            Console.WriteLine("N/A");
+        }
+
+        public void Exercise5()
+        {
+            Console.WriteLine("N/A");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using Mosh.ControlFlow;
+
+Exercises exercises = new Exercises();
+
+void InvalidEntry()
+{
+    Console.Clear();
+    Console.WriteLine("Invalid entry. Try again.");
+    Console.WriteLine();
+    ShowMenu();
+}
+void ShowMenu()
+{
+    int userSelection = -1;
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("****************************\n*     CHOOSE A FUNCTION    *\n* 1. Divisible by Three\n* 2. Ongoing Sum\n* 3. Compute Factorial\n* 4. Guess the Number\n* 5. Find the Maximum\n* 0. Exit the Application\n****************************");
+    Console.ForegroundColor = ConsoleColor.White;
+    string serialUserSelection = Console.ReadLine();
+    try
+    {
+        userSelection = Int32.Parse(serialUserSelection);
+    }
+    catch (Exception ex)
+    {
+        InvalidEntry();
+    }
+
+    switch (userSelection)
+    {
+        case 0:
+            Environment.Exit(0); break;
+
+        case 1:
+            exercises.Exercise1(); break;
+
+        case 2:
+            exercises.Exercise2(); break;
+
+        case 3:
+            exercises.Exercise3(); break;
+
+        case 4:
+            exercises.Exercise4(); break;
+
+        case 5:
+            exercises.Exercise5(); break;
+
+        default:
+            InvalidEntry();
+            break;
+    }
+}
+
+ShowMenu();
